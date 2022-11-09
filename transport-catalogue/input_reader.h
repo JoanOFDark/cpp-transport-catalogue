82525801373a1e8f22369acea26bee6c3467c2e2
JoanOFDark <87716444+JoanOFDark@users.noreply.github.com>
2022-11-09 10:52:23 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,17 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"    // структуры данных
+
+#include <utility>          // for std::pair<>
+#include <string>
+#include <string_view>      // for string manipulation via sv
+#include <istream>          // for istream
+#include <iostream>         // for cout  (in input_reader)
+
+namespace transport_catalogue::input_reader
+{
+
+	// Производит построчное чтение исходных данных согласно заданной схеме
+	// и формирует список запросов на обновление каталога
+	void ProcessInput(TransportCatalogue&, std::istream&);
+
+}
