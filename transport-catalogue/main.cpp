586a5db37feea90af804dabc1e950a632b56202e
JoanOFDark <87716444+JoanOFDark@users.noreply.github.com>
2022-11-09 10:57:12 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,16 @@
-// место для вашего кода
+#include "geo.h"
+#include "input_reader.h"
+#include "transport_catalogue.h"
+#include "stat_reader.h"
+
+#include <sstream>
+
+int main()
+{
+    transport_catalogue::TransportCatalogue tc;
+
+    transport_catalogue::input_reader::ProcessInput(tc, std::cin);
+    transport_catalogue::stat_reader::ProcessRequest(tc, std::cin);
+
+    return 0;
+}
