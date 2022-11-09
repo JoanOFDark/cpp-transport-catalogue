9ab3ffdc1553bd99135cf9255fba47357c793ba5
JoanOFDark <87716444+JoanOFDark@users.noreply.github.com>
2022-11-09 10:53:44 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,16 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"    // структуры данных
+
+#include <utility>          // for std::pair<>
+#include <string>
+#include <string_view>      // for string manipulation via sv
+#include <istream>          // for istream
+#include <iostream>         // for cout
+
+namespace transport_catalogue::stat_reader
+{
+
+	// Производит чтение запросов к справочнику
+	void ProcessRequest(TransportCatalogue&, std::istream&);
+
+}
