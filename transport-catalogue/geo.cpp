25a1370f9970dbe6d9ba12cbd0627ef097e08306
Ivan Kryloff <87716444+JoanOFDark@users.noreply.github.com>
2022-12-17 12:01:40 +0300

    Delete transport-catalogue directory

--- transport-catalogue/geo.cpp
+++
@@ -1,9 +0,0 @@
-#include "geo.h" 
-
-bool TransportsCatalogue::detail::distant::Coordinates:: operator==(const Coordinates& other) const {
-    return lat == other.lat && lng == other.lng;
-}
-
-bool  TransportsCatalogue::detail::distant::Coordinates::operator!=(const Coordinates& other) const {
-    return !(*this == other);
-}
